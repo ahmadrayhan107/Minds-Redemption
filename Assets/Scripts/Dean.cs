a449c5c402b986ef9f15aa224f32d87ba6662644
AhmadRayhan107 <ahmadrayhan45732@gmail.com>
2023-06-01 15:56:57 +0700

    Update jump sprite

--- Assets/Scripts/Dean.cs
+++ Assets/Scripts/Dean.cs
@@ -9,7 +9,15 @@ public class Dean : MonoBehaviour
 
     private Animator animator;
 
+    [SerializeField] private Transform groundCheck;
+
+    [SerializeField] private float groundCheckRadius = 0.05f;
+
     [SerializeField] private float speed = 2f;
+
+    [SerializeField] private float jumpForce = 10f;
+
+    [SerializeField] private LayerMask collisionMask;
 
     // Start is called before the first frame update
     void Start()
@@ -22,14 +30,26 @@ public class Dean : MonoBehaviour
     void Update()
     {
         var inputX = Input.GetAxisRaw("Horizontal");
+        var jumpInput = Input.GetButton("Jump"); 
 
         rigidBody.velocity = new Vector2(inputX * speed, rigidBody.velocity.y);
+
+        if(jumpInput && IsGrounded())
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+        }
 
         if (inputX != 0)
         {
             transform.localScale = new Vector3(Mathf.Sign(inputX), 1, 1);
         }
 
+        animator.SetBool("IsJump", IsGrounded());
         animator.SetBool("IsRun", inputX != 0); 
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionMask);
     }
 }
