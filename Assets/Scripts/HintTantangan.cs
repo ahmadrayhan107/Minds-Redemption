b5b4a259efb66508b527dcc663da24130ab1b1b9
AhmadRayhan107 <ahmadrayhan45732@gmail.com>
2023-06-09 09:05:32 +0700

    Update Responsive Hint

--- Assets/Scripts/HintTantangan.cs
+++ Assets/Scripts/HintTantangan.cs
@@ -5,17 +5,20 @@ using UnityEngine.SceneManagement;
 public class HintTantangan : MonoBehaviour
 {
     // Start is called before the first frame update
-   public void LoadObstacle1(){
-    SceneManager.LoadScene("Level-1-1");
-   }
-
-   public void LoadObstacle2(){
-    SceneManager.LoadScene("Level-1-2");
-   }
-   public void LoadObstacle3(){
-    SceneManager.LoadScene("Level-2-1");
-   }
-   public void LoadObstacle4(){
-    SceneManager.LoadScene("Level-2-2");
-   }
+    public void LoadObstacle1()
+    {
+        SceneManager.LoadScene("Level-1-1");
+    }
+    public void LoadObstacle2()
+    {
+        SceneManager.LoadScene("Level-1-2");
+    }
+    public void LoadObstacle3()
+    {
+        SceneManager.LoadScene("Level-2-1");
+    }
+    public void LoadObstacle4()
+    {
+        SceneManager.LoadScene("Level-2-2");
+    }
 }
