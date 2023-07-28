76facc68948e4116b1c0ac4f609e9de00a395cc0
Aditya Ibnu Arif <akunkuliahadit@gmail.com>
2023-07-28 09:36:03 +0700

    ganti masuk berdua

--- Assets/Scripts/Level_1_2.cs
+++ Assets/Scripts/Level_1_2.cs
@@ -5,9 +5,45 @@ using UnityEngine.SceneManagement;
 
 public class Level_1_2 : MonoBehaviour
 {
-    public void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Player2")) {
+    private bool player1Touched = false;
+    private bool player2Touched = false;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player1"))
+        {
+            player1Touched = true;
+        }
+        else if (other.CompareTag("Player2"))
+        {
+            player2Touched = true;
+        }
+
+        CheckPlayerStatus();
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player1"))
+        {
+            player1Touched = false;
+        }
+        else if (other.CompareTag("Player2"))
+        {
+            player2Touched = false;
+        }
+    }
+
+    private void CheckPlayerStatus()
+    {
+        if (player1Touched && player2Touched)
+        {
             SceneManager.LoadScene("HintTantangan2");
+            Debug.Log("berhasil");
+        }
+        else
+        {
+            Debug.Log("gagal");
         }
     }
 }
